0dd889f8220ab66cce50bcaec7519713be78f77b
Juun <bluejun0510@naver.com>
2022-06-20 13:24:10 +0900

    Feat add simple components

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,21 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  staticDirs: ["../public"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-interactions",
+  stories: [
+     '../src/**/*.stories.mdx',
+     '../src/**/*.stories.@(js|jsx|ts|tsx)',
+     '../src/components/**/*.stories.js'
   ],
-  features: {
-    postcss: false,
-  },
-  framework: "@storybook/react",
-  core: {
-    builder: "webpack4",
-  },
-};
+    staticDirs: ['../public'],
+    addons: [
+      '@storybook/addon-links',
+      '@storybook/addon-essentials',
+      '@storybook/preset-create-react-app',
+      '@storybook/addon-interactions',
+    ],
+    features: {
+      postcss: false,
+    },
+    framework: '@storybook/react',
+    core: {
+      builder: 'webpack4',
+    },
+  };
